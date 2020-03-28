a78bafc5d8a7c1d19966f6e9b225c2ed620fb10d
Camilo Barbosa <cab331@hotmail.com>
2020-03-28 18:34:10 -0500

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 var path = require('path');
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'public/dist'),
-      filename: '[name].js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react']
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
           }
         }
-      ]
-    }
+      }
+    ]
   }
+}
