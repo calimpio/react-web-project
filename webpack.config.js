cb267c6a00bdc66d544258a8dd534685139e2783
Camilo Barbosa <cab331@hotmail.com>
2020-03-28 11:35:13 -0500

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-      path: path.resolve(__dirname, 'public'),
+      path: path.resolve(__dirname, 'public/dist'),
       filename: '[name].js'
     },
     module: {
