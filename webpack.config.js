8f7ec669e22f4d4d38681ecde0b7232c25cf99fb
Camilo Barbosa <cab331@hotmail.com>
2020-03-28 11:29:57 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
+          }
+        }
+      ]
+    }
+  }
